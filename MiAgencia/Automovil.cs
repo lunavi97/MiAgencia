b7e3a4b409ae5fd553d16bc6417f7c3e47a56e2d
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-14 00:24:44 -0300

    Definición de clases que heredan de la superclase Vehiculo

--- MiAgencia/Automovil.cs
+++ MiAgencia/Automovil.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MiAgencia
 {
-    class Automovil
+    class Automovil : Vehiculo
     {
     }
 }
--- MiAgencia/Camioneta.cs
+++ MiAgencia/Camioneta.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MiAgencia
 {
-    class Camioneta
+    class Camioneta : Vehiculo
     {
     }
 }
