37011712d65824a78f0379be3e9268836fa28ffc
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-14 00:48:45 -0300

    Establecidos atributos y métodos de la superclase Vehiculo

--- MiAgencia/Vehiculo.cs
+++ MiAgencia/Vehiculo.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace MiAgencia
 {
+    public enum ColorVehiculo
+    {
+        Ninguno,
+        Blanco,
+        Gris,
+        Rojo,
+        Vino,
+        Azul,
+        Negro
+    };
+
     public class Vehiculo
     {
+        #region Atributos
+        int id;
+        int llantas;
+        string modelo;
+        int anio;
+        ColorVehiculo color;
+        #endregion
+
+        #region Métodos
+        public string Capturar()
+        {
+            // Capturar en BD
+            return "Capturado";
+        }
+
+        public string Vender()
+        {
+            // Registrar la venta en BD
+            return "Vendido";
+        }
+        #endregion
     }
 }
